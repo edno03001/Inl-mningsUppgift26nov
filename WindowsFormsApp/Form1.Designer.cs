ba2186149576227bea7b737b0f67568462981f5b
edno03001 <73816539+edno03001@users.noreply.github.com>
2020-11-23 10:03:47 +0100

    Påbörjade winforms, fast vid jämförning av talen

---
+++ WindowsFormsApp/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace WindowsFormsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.bSlumpaTal = new System.Windows.Forms.Button();
+            this.bJämförTal = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBoxSvar = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            this.contextMenuStrip1.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip1_Opening);
+            // 
+            // bSlumpaTal
+            // 
+            this.bSlumpaTal.Location = new System.Drawing.Point(325, 39);
+            this.bSlumpaTal.Name = "bSlumpaTal";
+            this.bSlumpaTal.Size = new System.Drawing.Size(141, 84);
+            this.bSlumpaTal.TabIndex = 2;
+            this.bSlumpaTal.Text = "Generera ett tal";
+            this.bSlumpaTal.UseVisualStyleBackColor = true;
+            this.bSlumpaTal.Click += new System.EventHandler(this.bSlumpaTal_Click);
+            // 
+            // bJämförTal
+            // 
+            this.bJämförTal.Location = new System.Drawing.Point(325, 241);
+            this.bJämförTal.Name = "bJämförTal";
+            this.bJämförTal.Size = new System.Drawing.Size(141, 61);
+            this.bJämförTal.TabIndex = 3;
+            this.bJämförTal.Text = "Jämför talen";
+            this.bJämförTal.UseVisualStyleBackColor = true;
+            this.bJämförTal.Click += new System.EventHandler(this.bJämförTal_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
+            this.richTextBox1.Location = new System.Drawing.Point(344, 185);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(100, 50);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(635, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // richTextBoxSvar
+            // 
+            this.richTextBoxSvar.Location = new System.Drawing.Point(172, 308);
+            this.richTextBoxSvar.Name = "richTextBoxSvar";
+            this.richTextBoxSvar.Size = new System.Drawing.Size(478, 96);
+            this.richTextBoxSvar.TabIndex = 7;
+            this.richTextBoxSvar.Text = "";
+            this.richTextBoxSvar.TextChanged += new System.EventHandler(this.richTextBoxSvar_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBoxSvar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.bJämförTal);
+            this.Controls.Add(this.bSlumpaTal);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Button bSlumpaTal;
+        private System.Windows.Forms.Button bJämförTal;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBoxSvar;
+    }
+}
+
---
+++ WindowsFormsApp/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int a;
+        int b = 0;
+        string stringb;
+        int c = 1;
+        int d = 0;
+
+        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bSlumpaTal_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            a = rnd.Next(1, 100);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            Console.WriteLine(a);
+        }
+
+        private void richTextBox2_TextChanged(object sender, EventArgs e)
+        {
+            Console.WriteLine(a);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            label1.Text = Convert.ToString(a);
+        }
+
+        private void richTextBox1_TextChanged_1(object sender, EventArgs e)
+        {
+            richTextBox1.Text = stringb;
+        }
+        private void bJämförTal_Click(object sender, EventArgs e)
+        {
+            b = Convert.ToInt32(stringb);
+            if (b < a)
+                Console.WriteLine("Ditt tal är för litet, försök igen");
+            else if (b > a)
+                Console.WriteLine("Ditt tal är för stort, försök igen");
+
+            d = c++;
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBoxSvar_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
